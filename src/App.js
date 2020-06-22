062e038ee77d50e3de978e2f26713c3db4decfce
admodev <admosdigital@yahoo.com>
2020-06-22 00:37:02 -0300

    Iniciar proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Adolfo Moyano</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Portfolio</Nav.Link>
+          <Nav.Link href="#features">Mi Historia</Nav.Link>
+          <Nav.Link href="#pricing">Contacto</Nav.Link>
+        </Nav>
+      </Navbar>
     </div>
   );
 }
